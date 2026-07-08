31779f24dbbfc115eaecf26c2d9e878e32fc0746
alex-bleotu <alexbleotu2006@gmail.com>
2026-07-08 23:34:39 +0300

    chore: bump to v1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.alexbleotu.vault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
