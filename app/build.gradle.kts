8ee80b38fd1a51e06c959c7ed0ee386a3f0f3935
alex-bleotu <alexbleotu2006@gmail.com>
2026-08-17 15:44:46 +0300

    fix: friend-reel exemption breaking on reel-to-reel preload, home-feed scroll limiter breaking on inline feed reels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.alexbleotu.vault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.20"
+        versionCode = 21
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
